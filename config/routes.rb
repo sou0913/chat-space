25f76f162cb559f3d52184bcb566e32a04d0cf67
sou0913 <usokisasa0913@gmail.com>
2019-09-26 12:30:01 +0900

    changed root path, and made index of group

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
-  root to: 'messages#index'
+  root to: 'groups#index'
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
