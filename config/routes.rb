96883e079eee5a42643169683c6930b7c2be1208
sou0913 <usokisasa0913@gmail.com>
2019-09-30 22:06:19 +0900

    intruduce incremental search into group ceate&update system.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index,:edit, :update]
   root to: 'groups#index'
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
